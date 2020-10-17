f0d558c5e0554b55fe60ae05779b123095313c92
gbss2 <jwojwo@gmail.com>
2020-10-17 17:57:06 -0300

    Create kaas_data_retrieval.py

---
+++ kaas_data_retrieval.py
@@ -0,0 +1,25 @@
+======================================================================
+# Bio Bureau Project
+#=====================================================================
+#
+# (/u0254) Copyleft 2020, by Bio Bureau and Contributors.
+#
+# -----------------
+# kaas_data_retrieval
+# -----------------
+# GNU GPL 2020, by Bio Bureau and Contributors.
+#
+# ORIGINAL AUTHOR: Giordano Bruno Soares-Souza
+# SOURCES AND/OR CONTRIBUTOR(S):
+#
+# UPDATED BY: Giordano Bruno Soares-Souza
+#
+# COMMAND LINE: ./kaas_data_retrieval.py -out <output name> -url <kaas result URL>
+#
+# DEPENDENCIES:
+# DESCRIPTION:
+#
+# INPUTS: KEGG KAAS URL results
+# FUTURE DEVELOPMENTS:
+# 1)
+################################################################################
