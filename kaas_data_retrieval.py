a22c8021bd1207b3f692b6fd55d01759b68eed53
gbss2 <jwojwo@gmail.com>
2020-10-18 22:15:59 -0300

    Update kaas_data_retrieval.py

--- kaas_data_retrieval.py
+++ kaas_data_retrieval.py
@@ -88,7 +88,7 @@ def writeOutput(outputBasename, resPage, resTree, pathways, pathways1, pathways2
         print('/n/n/#######Pathway3', file=l)
         print(tuple(pathways3), file=l)
         print('/n/n/#######Table', file=l)
-        pdPathway = pd.DataFrame(pathways3, columns = ['Pathway', 'Link'], index=list(pathways1))
+        pdPathway = pd.DataFrame(list(pathways3), columns = ['Pathway', 'Link'], index=list(pathways1))
         print(pdPathway, file=l)
         print('/n/n/#######Table', file=l)
         print(pd.DataFrame(list(pathways3), columns = ['Pathway', 'Link']), file=l)
